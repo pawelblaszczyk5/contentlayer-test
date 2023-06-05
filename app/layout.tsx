a4db762fca66c89514c32d89a9d4fd934f07e6f5
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-05 21:40:35 +0200

    Add runtime to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "@/app/globals.css";
 
+export const runtime = "experimental-edge";
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => (
 	<html lang="en">
 		<body>{children}</body>
