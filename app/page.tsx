b3258acabf5b681b8c8d6b244921bf1d57e33560
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-13 18:02:15 +0200

    Revalidation with separate fetch call

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import { allPosts } from "contentlayer/generated";
 
-const HomePage = () => (
-	<div>
-		{allPosts.map(post => (
-			<h1 key={post._id}>{post.title}</h1>
-		))}
-	</div>
-);
+const HomePage = async () => {
+	const data = (await (
+		await fetch("https://csrng.net/csrng/csrng.php?min=0&max=100", { next: { revalidate: 30 } })
+	).json()) as Array<{ random: number }>;
+
+	return (
+		<>
+			<p>Test {data[0]?.random}</p>
+			<div>
+				{allPosts.map(post => (
+					<h1 key={post._id}>{post.title}</h1>
+				))}
+			</div>
+		</>
+	);
+};
 
 export default HomePage;
